b34ddb80e828b8032ce5a4b2e8c360f45f9f7b76
ConfusedPolarBear <33811686+ConfusedPolarBear@users.noreply.github.com>
2021-09-16 18:04:17 -0500

    Update App.vue and index.ts

--- backend/web/src/App.vue
+++ backend/web/src/App.vue
@@ -1,20 +1,7 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        img
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      Garden Management
     </v-app-bar>
 
     <v-main>
