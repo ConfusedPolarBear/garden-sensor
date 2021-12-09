a0df1cf76f0303b2de6378c60c0ea6f5f03c14ce
ConfusedPolarBear <33811686+ConfusedPolarBear@users.noreply.github.com>
2021-12-09 02:44:10 -0600

    Humanize last pushed durations

--- frontend/src/components/NodeModule.vue
+++ frontend/src/components/NodeModule.vue
@@ -31,8 +31,10 @@
             </h2>
           </span>
           <p class="secondary-text">
-            Last pushed {{ timestamp }} seconds ago
-            <v-icon class="icon" small> mdi-clock </v-icon>
+            <tooltip :text="lastSeen">
+              Last pushed {{ humanize(timestamp) }}
+              <v-icon class="icon" small> mdi-clock </v-icon>
+            </tooltip>
           </p>
         </v-col>
       </v-row>
@@ -48,6 +50,18 @@ import Tooltip from "@/components/Tooltip.vue";
 export default Vue.extend({
   name: "NodeModule",
   components: { Tooltip },
+  data() {
+    return {
+      dateOptions: {
+        hour12: false
+      } as Intl.DateTimeFormatOptions,
+
+      // Maximum number of humanized duration portions to display. Higher values will be more accurate but
+      // take up more space. For example, if a node was last seen 4 weeks ago, do most users really care
+      // that it was *exactly* 4 weeks, 11 days, 13 hours, 7 minutes, 2 seconds ago?
+      maximumParts: 2
+    };
+  },
   props: [
     "moduleName",
     "identifier",
@@ -59,11 +73,91 @@ export default Vue.extend({
     meshInfo(announcement: GardenSystemInfo): string {
       const mesh = announcement.IsMesh;
       return mesh ? "Mesh" : `MQTT (CH ${announcement.Channel})`;
+    },
+    humanize(raw: number): string {
+      /* Example: suppose raw is 436507 (seconds).
+       * Step 1: floor(436507 / 86400) = 5 days.
+       * raw -= 5*86400. raw is now 4507
+       * Step 2: floor(4507 / 3600) = 1 hour.
+       * raw -= 1*3600. raw is now 907
+       * Step 3: floor(907 / 60) = 15 minutes.
+       * raw -= 15*60. raw is now 7 seconds.
+       *
+       * For all pushed parts, check if they need to be pluralized (almost certainly since only 1 doesn't need it).
+       * Combine those parts into the string "5 days, 1 hour, 15 minutes, 7 seconds".
+       * This is correct since 7 + 15*60 + 1*60*60 + 5*60*60*24 = 436507, which is exactly equal to the input value.
+       */
+
+      if (raw <= 2) {
+        return "now";
+      }
+
+      interface duration {
+        n: number;
+        s: string;
+      }
+
+      let parts: Array<duration> = [];
+
+      const steps = [
+        {
+          n: 60 * 60 * 24,
+          s: "day"
+        },
+        {
+          n: 60 * 60,
+          s: "hour"
+        },
+        {
+          n: 60,
+          s: "minute"
+        },
+        {
+          n: 1,
+          s: "second"
+        }
+      ] as Array<duration>;
+
+      // Convert the seconds into a human readable duration.
+      for (const step of steps) {
+        if (parts.length >= this.maximumParts) {
+          break;
+        }
+
+        const tmp = Math.floor(raw / step.n);
+        if (tmp == 0) {
+          continue;
+        }
+
+        parts.push({ n: tmp, s: step.s });
+        raw -= tmp * step.n;
+      }
+
+      // Fix grammar.
+      let humanized = "";
+      for (let i = 0; i < parts.length; i++) {
+        const p = parts[i];
+        if (p.n > 1) {
+          p.s += "s";
+        }
+
+        humanized += `${p.n} ${p.s}, `;
+      }
+
+      // Remove trailing comma.
+      return humanized.substring(0, humanized.length - 2) + " ago";
     }
   },
   computed: {
     state(): string {
       return this.isConnected ? "connected" : "disconnected";
+    },
+    lastSeen(): string {
+      const t = this.$props.timestamp * 1000; // seconds -> ms
+      return new Date(new Date().getTime() - t).toLocaleString(
+        undefined,
+        this.dateOptions
+      );
     }
   }
 });
