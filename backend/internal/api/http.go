d021e1cb94670412bea65574c5b6d9900dc34968
ConfusedPolarBear <33811686+ConfusedPolarBear@users.noreply.github.com>
2021-12-17 03:45:53 -0600

    Separate out send command logic

--- backend/internal/api/http.go
+++ backend/internal/api/http.go
@@ -1,8 +1,6 @@
 package api
 
 import (
-	"bytes"
-	"crypto/rand"
 	"fmt"
 	"io"
 	"net/http"
@@ -11,10 +9,8 @@ import (
 	"strings"
 
 	"github.com/ConfusedPolarBear/garden/internal/db"
-	"github.com/ConfusedPolarBear/garden/internal/mqtt"
 	"github.com/ConfusedPolarBear/garden/internal/util"
 	"github.com/ConfusedPolarBear/garden/internal/websocket"
-	"golang.org/x/crypto/chacha20poly1305"
 
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
@@ -31,7 +27,7 @@ func StartServer() {
 	r.HandleFunc("/systems", GetSystems).Methods("GET")
 	r.HandleFunc("/system/{id}", GetSystem).Methods("GET")
 	r.HandleFunc("/system/delete/{id}", DeleteSystem).Methods("POST")
-	r.HandleFunc("/system/command/{id}", SendCommand).Methods("POST", "OPTIONS")
+	r.HandleFunc("/system/command/{id}", SendCommandHandler).Methods("POST", "OPTIONS")
 	r.HandleFunc("/system/update/{id}", StartOTA).Methods("POST")
 
 	r.HandleFunc("/firmware/manifest.json", ManifestHandler).Methods("GET")
@@ -101,7 +97,7 @@ func DeleteSystem(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNoContent)
 }
 
-func SendCommand(w http.ResponseWriter, r *http.Request) {
+func SendCommandHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := getId(w, r)
 	if err != nil {
 		return
@@ -120,104 +116,12 @@ func SendCommand(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// If the user wants to encrypt the command, do that now.
-	if r.Form.Has("encrypt") {
-		// Mesh messages are limited to 212 bytes in size. The nonce (12) + tag (16) drop that limit down to 184 bytes.
-		if len(command) > 184 {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
+	encrypt := r.Form.Has("encrypt")
 
-		config, err := db.GetConfiguration()
-		if err != nil {
-			logrus.Warnf("[server] unable to get configuration: %s", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		// Create the cipher.
-		// TODO: should be cached.
-		chacha, err := chacha20poly1305.New(config.ChaChaKey)
-		if err != nil {
-			logrus.Warnf("[crypto] unable to initialize cipher: %s", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		// Create the nonce and seal the box.
-		// Ensure that nulls and carriage returns aren't present because the C++ firmware can't handle them correctly
-		// and it will fail to authenticate the ciphertext.
-		// TODO: *properly* fix this.
-		var nonce, raw []byte
-		forbidden := "\x00\x0d"
-
-		for {
-			nonce, raw = nil, nil
-
-			nonce = make([]byte, chacha20poly1305.NonceSize)
-			if _, err := rand.Read(nonce); err != nil {
-				panic(err)
-			}
-
-			raw = chacha.Seal(raw, nonce, []byte(command), nil)
-
-			if !bytes.ContainsAny(nonce, forbidden) && !bytes.ContainsAny(raw, forbidden) {
-				break
-			}
-		}
-
-		// Garden systems expect nonce + tag + ciphertext, not nonce + ciphertext + tag. Swap the bytes to account for this.
-		start := len(raw) - 16
-		tag, ciphertext := raw[start:], raw[:start]
-
-		command = fmt.Sprintf("e%s%s%s", nonce, tag, ciphertext)
-
-		// logrus.Debugf("nonce is %X, tag is %X, and ciphertext is %X", nonce, tag, ciphertext)
+	if err := sendCommand(id, command, encrypt); err != nil {
+		logrus.Warnf("[server] unable to send command: %s", err)
+		w.WriteHeader(http.StatusBadRequest)
 	}
-
-	// Get the system
-	isMesh := false
-	if id != "FFFFFFFFFFFF" {
-		// If this is not a broadcast message, lookup the individual system to send the message to
-		system, err := db.GetSystem(id, false)
-		if err != nil {
-			w.WriteHeader(http.StatusNotFound)
-			return
-		}
-
-		isMesh = system.Announcement.IsMesh
-	} else {
-		isMesh = true
-	}
-
-	logrus.Debugf("[server] sending command to %s: %s", id, command)
-
-	mqttDest := id
-	mqttPayload := command
-
-	// If this system is connected over MQTT, send the raw command
-	if isMesh {
-		// Mesh connected systems are controlled by sending a command (MQTT) to the coordinator who will rebroadcast it (ESP-NOW)
-		coordinator, err := db.GetCoordinator()
-		if err != nil {
-			logrus.Errorf("[server] unable to find coordinator: %s", err)
-			w.WriteHeader(http.StatusNotFound)
-			return
-		}
-
-		mqttDest = coordinator.Identifier
-
-		// +12 bytes for the MAC address and +4 bytes for "dst-"
-		logrus.Debugf("[server] mesh payload will be %d bytes long", len(mqttPayload)+12+4)
-
-		// Construct the mesh payload
-		mqttPayload = fmt.Sprintf(`{"Command":"Publish","Payload":"h%x"}`, "dst-"+id+mqttPayload)
-	}
-
-	logrus.Debugf("[server] commanding \"%s\"", mqttDest)
-	logrus.Debugf("[server] mqtt payload \"%s\"", mqttPayload)
-
-	mqtt.Publish(fmt.Sprintf("garden/module/%s/cmnd", mqttDest), mqttPayload)
 }
 
 func StartOTA(w http.ResponseWriter, r *http.Request) {
@@ -326,6 +230,9 @@ func StartOTA(w http.ResponseWriter, r *http.Request) {
 	ota.URL = fmt.Sprintf("%s/%s", host, shortCode)
 	logrus.Debugf("[server] set OTA url to %s (used host %s)", ota.URL, host)
 
-	// TODO: publish the command directly
-	w.Write(util.Marshal(ota))
+	if err := sendCommand(id, string(util.Marshal(ota)), true); err != nil {
+		logrus.Warnf("[server] unable to initiate OTA for %s: %s", id, err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 }
